a0f0783c49d998a12c66ecdf1ea00ddcfb9b6b17
pilot114 <pilot114@bk.ru>
2019-07-16 00:55:00 +0700

    2-4 lessons

--- lesson_003/Modal.js
+++ lesson_003/Modal.js
@@ -1,11 +1,11 @@
-class Modal{
-	constructor(meshData){
-		//TODO :: create this.transform in future lesson
-		this.mesh = meshData;
-	}
+class Modal {
+    constructor(meshData) {
+        //TODO :: create this.transform in future lesson
+        this.mesh = meshData;
+    }
 
-	//Things to do before its time to render
-	preRender(){ 
-		//TODO :: Update transform Matrix in future lesson
-	}
+    //Things to do before its time to render
+    preRender() {
+        //TODO :: Update transform Matrix in future lesson
+    }
 }
