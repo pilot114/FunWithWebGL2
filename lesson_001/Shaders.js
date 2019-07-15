399819df5fbf14ac181bda45f8e37e0b628d0b75
chulkov <pilot114@bk.ru>
2019-07-15 21:00:21 +0700

    1 lesson

--- lesson_001/Shaders.js
+++ lesson_001/Shaders.js
@@ -1,54 +1,59 @@
-class ShaderUtil{
-	static domShaderSrc(elmID){
-		var elm = document.getElementById(elmID);
-		if(!elm || elm.text == ""){ console.log(elmID + " shader not found or no text."); return null; }
-		
-		return elm.text;
-	}
+class ShaderUtil {
+    static domShaderSrc(elmID) {
+        var elm = document.getElementById(elmID);
+        if (!elm || elm.text == "") {
+            console.log(elmID + " shader not found or no text.");
+            return null;
+        }
 
-	static createShader(gl,src,type){
-		var shader = gl.createShader(type);
-		gl.shaderSource(shader,src);
-		gl.compileShader(shader);
+        return elm.text;
+    }
 
-		//Get Error data if shader failed compiling
-		if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-			console.error("Error compiling shader : " + src, gl.getShaderInfoLog(shader));
-			gl.deleteShader(shader);
-			return null;
-		}
+    static createShader(gl, src, type) {
+        var shader = gl.createShader(type);
+        gl.shaderSource(shader, src);
+        gl.compileShader(shader);
 
-		return shader;
-	}
+        //Get Error data if shader failed compiling
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error("Error compiling shader : " + src, gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null;
+        }
 
-	static createProgram(gl,vShader,fShader,doValidate){
-		//Link shaders together
-		var prog = gl.createProgram();
-		gl.attachShader(prog,vShader);
-		gl.attachShader(prog,fShader);
-		gl.linkProgram(prog);
+        return shader;
+    }
 
-		//Check if successful
-		if(!gl.getProgramParameter(prog, gl.LINK_STATUS)){
-			console.error("Error creating shader program.",gl.getProgramInfoLog(prog));
-			gl.deleteProgram(prog); return null;
-		}
+    static createProgram(gl, vShader, fShader, doValidate) {
+        //Link shaders together
+        var prog = gl.createProgram();
+        gl.attachShader(prog, vShader);
+        gl.attachShader(prog, fShader);
+        gl.linkProgram(prog);
 
-		//Only do this for additional debugging.
-		if(doValidate){
-			gl.validateProgram(prog);
-			if(!gl.getProgramParameter(prog,gl.VALIDATE_STATUS)){
-				console.error("Error validating program", gl.getProgramInfoLog(prog));
-				gl.deleteProgram(prog); return null;
-			}
-		}
-		
-		//Can delete the shaders since the program has been made.
-		gl.detachShader(prog,vShader); //TODO, detaching might cause issues on some browsers, Might only need to delete.
-		gl.detachShader(prog,fShader);
-		gl.deleteShader(fShader);
-		gl.deleteShader(vShader);
+        //Check if successful
+        if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+            console.error("Error creating shader program.", gl.getProgramInfoLog(prog));
+            gl.deleteProgram(prog);
+            return null;
+        }
 
-		return prog;
-	}
+        //Only do this for additional debugging.
+        if (doValidate) {
+            gl.validateProgram(prog);
+            if (!gl.getProgramParameter(prog, gl.VALIDATE_STATUS)) {
+                console.error("Error validating program", gl.getProgramInfoLog(prog));
+                gl.deleteProgram(prog);
+                return null;
+            }
+        }
+
+        //Can delete the shaders since the program has been made.
+        gl.detachShader(prog, vShader); //TODO, detaching might cause issues on some browsers, Might only need to delete.
+        gl.detachShader(prog, fShader);
+        gl.deleteShader(fShader);
+        gl.deleteShader(vShader);
+
+        return prog;
+    }
 }
